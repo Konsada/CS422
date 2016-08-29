62bef04516b94e39bb1d1de106a482e6db431b74
Konsada <keon.sadatian@gmail.com>
2016-08-28 22:19:04 -0700

    Starting Stream classes

---
+++ CS422/IndexedNumsStream.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace CS422
+{
+    public class IndexedNumsStream : Stream
+    {
+        private int currentPosition;
+        private long streamLength;
+        IndexedNumsStream(long length)
+        {
+            streamLength = length;
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool CanSeek
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    if(value > streamLength)
+                    {
+                        Position = streamLength;
+                    }
+                    else
+                    {
+                        Position = value;
+                    }
+                }
+                else
+                    Position = 0;
+            }
+        }
+
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (count < 0)
+                count = 0;
+            for(int i = offset; i < count; i++)
+            {
+                buffer[i] =(byte)(i % 256);
+            }
+            return count;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            if (value < 0)
+                streamLength = 0;
+            else
+                streamLength = value;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CS422/NumberedTextWriter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+
+namespace CS422
+{
+    public class NumberedTextWriter : TextWriter
+    {
+        private int currentLineNumber;
+        private TextWriter _wrapThis;
+        private string someString;
+        NumberedTextWriter(TextWriter wrapThis)
+        {
+            currentLineNumber = 1;
+        }
+        NumberedTextWriter(TextWriter wrapThis, int startingLineNumber)
+        {
+            currentLineNumber = startingLineNumber;
+        }
+        public override Encoding Encoding
+        {
+            get { return _wrapThis.Encoding; }
+        }
+        public override void WriteLine(string value)
+        {
+            _wrapThis.Write(currentLineNumber.ToString() + ": " + value);
+            currentLineNumber++;
+        }
+    }
+}
